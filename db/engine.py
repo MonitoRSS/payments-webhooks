82a063dd9089b24618db61ea6a9663f866cc82e8
synzen <michael994664@gmail.com>
2020-12-28 20:04:47 -0500

    Add POSTGRES_URI constant

--- db/engine.py
+++ db/engine.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from utils.constants import POSTGRES_URI
 
 Base = declarative_base()
-engine = create_engine('sqlite:///test.db', echo=True)
+engine = create_engine(POSTGRES_URI, echo=True)
 
 Session = sessionmaker(bind=engine)
