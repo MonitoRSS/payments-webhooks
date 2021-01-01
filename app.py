d14bcdd91051523455c3a519e2b0e9ffbb6ea463
synzen <michael994664@gmail.com>
2020-12-31 21:47:35 -0500

    Fix mongo not connecting on flask start

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import stripe
 from flask import Flask
 from utils.constants import POSTGRES_URI, STRIPE_SECRET_API_KEY
 from routes.api import api
+import db.mongo
 stripe.api_key = STRIPE_SECRET_API_KEY
 
 app = Flask(__name__)
