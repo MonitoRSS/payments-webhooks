d98f728f975fc4da870e02c2cc5bd7a671721b8f
synzen <michael994664@gmail.com>
2021-01-01 12:02:23 -0500

    Fix env vars not being loaded in

--- utils/constants.py
+++ utils/constants.py
@@ -1,13 +1,15 @@
 import sys
 import os
+from dotenv import load_dotenv
 
-FLASK_ENV = os.getenv("FLASK_ENV")
-FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
-STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
-STRIPE_SECRET_API_KEY = os.getenv("STRIPE_SECRET_API_KEY")
-POSTGRES_URI = 'sqlite:///test.db' if 'pytest' in sys.modules else os.getenv(
-    "POSTGRES_URI")
-MONGODB_URI = "mongomock://localhost" if 'pytest' in sys.modules else os.getenv(
-    "MONGODB_URI")
-MONGODB_DATABASE = 'test' if 'pytest' in sys.modules else os.getenv(
-    "MONGODB_DATABASE")
+load_dotenv()
+
+FLASK_ENV = os.environ["FLASK_ENV"]
+STRIPE_WEBHOOK_SECRET = os.environ["STRIPE_WEBHOOK_SECRET"]
+STRIPE_SECRET_API_KEY = os.environ["STRIPE_SECRET_API_KEY"]
+POSTGRES_URI = 'sqlite:///test.db' if 'pytest' in sys.modules else os.environ[
+    "POSTGRES_URI"]
+MONGODB_URI = "mongomock://localhost" if 'pytest' in sys.modules else os.environ[
+    "MONGODB_URI"]
+MONGODB_DATABASE = 'test' if 'pytest' in sys.modules else os.environ[
+    "MONGODB_DATABASE"]
